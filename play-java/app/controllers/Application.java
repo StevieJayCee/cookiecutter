5bace13d13cfa15d292d5e80fa93ab2f4638cbfd
Glenn Sayers <glennsayers.gs@gmail.com>
2015-02-14 15:11:18 +0000

    Added styled login and payment screen

--- play-java/app/controllers/Application.java
+++ play-java/app/controllers/Application.java
@@ -7,7 +7,11 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok(index.render(TwillioService.generateTwillioAuthTokenForCalling()));
+    }
+
+    public static Result login() {
+        return ok(login.render("Your new application is ready."));
     }
 
     public static Result twillioCustomerCall() {
